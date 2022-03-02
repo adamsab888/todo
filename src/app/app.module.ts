caddee3fe91deb34892cdb7ee8f63aa24deb183c
Adam Sabo <adamsab44@hotmail.com>
2022-03-02 17:59:29 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {TodosModule} from "./todos/todos.module";
+import {AppRoutingModule} from "./app-routing/app-routing.module";
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main/main.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        TodosModule,
+        AppRoutingModule,
+
+
+    ],
+    providers: [],
+    exports: [
+
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {TodosService} from "../todos/todos/todos.service";
+
+@Component({
+  selector: 'app-todos-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  text: string = '';
+
+  changeText(event: Event): void {
+    const target = event.target as HTMLInputElement
+    this.text = target.value;
+  }
+
+  addTodo(): void {
+    this.todoService.addTodo(this.text);
+    this.text = '';
+
+  }
+
+  constructor(private todoService: TodosService) {}
+}
